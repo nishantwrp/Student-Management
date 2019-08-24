ec8e7b765e5817d77f5f5575c511bc0cf48236eb
Nishant Mittal <mittalnishant14@outlook.com>
2019-08-24 21:13:45 +0530

    Integrated Authentication API

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
 from .views import *
 urlpatterns = [
-    path('',indexView)
+    path('',indexView),
+    path('dashboard/',elementsView),
+    path('generic/',genView),
+    path('login/',loginView),
+    path('register/',registerView),
 ]
