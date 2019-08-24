a9114a89408fbe08b34f58371c0ed5f1f8e96abd
Mudit Bhardwaj <muditbhardwaj195@gmail.com>
2019-08-24 19:29:44 +0530

    login logout

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
 from .views import *
 urlpatterns = [
-    path('',indexView)
+    path('',indexView),
+    path('elements/', kuttaView),
+    path('generic/',genView),
+    path('login/',loginView),
+    path('register/',registerView),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def indexView(request):
     return render(request,'index.html')
+
+def kuttaView(request):
+    return render(request,'elements.html')
+
+def genView(request):
+    return render(request,'generic.html')
+
+def loginView(request):
+    return render(request,'login.html')
+
+def registerView(request):
+    return render(request,'register.html')
